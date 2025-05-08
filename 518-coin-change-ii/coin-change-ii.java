fdf88a8486a2f23f45b588da620b643ad3c3db2d
Amogha Manju <47893878+amoghamanju@users.noreply.github.com>
2025-05-08 15:08:42 -0500

    Time: 30 ms (6.89%) | Memory: 51.8 MB (7.01%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -1,14 +1,17 @@
 class Solution {
+    
     public int change(int amount, int[] coins) {
-        int[] dp=new int[amount+1];
-        dp[0]=1;
-        for(int coin:coins)
-        {
-            for(int i=coin;i<=amount;i++)
-            {
-                dp[i]+=dp[i-coin];
-            }
-        }
-        return dp[amount];
+        Integer[][]dp=new Integer[coins.length][amount+1];
+        return countways(coins,0,amount,dp);
+    }
+    private int countways(int[] coins,int index, int amount, Integer[][]dp)
+    {
+        if (amount==0) return 1;
+        if(index==coins.length || amount<0) return 0;
+        if(dp[index][amount]!=null) return dp[index][amount];
+        int pick=countways(coins,index,amount-coins[index],dp);
+        int notpick=countways(coins,index+1, amount,dp);
+
+        return dp[index][amount]=pick+notpick;
     }
 }
