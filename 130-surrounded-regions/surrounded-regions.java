6b8e5f20478b4e3bfe33f574474c90cf36fcee99
Amogha Manju <47893878+amoghamanju@users.noreply.github.com>
2025-06-10 20:11:27 -0500

    Time: 2 ms (83.71%) | Memory: 45.8 MB (27.86%) - LeetSync

---
+++ 130-surrounded-regions/surrounded-regions.java
@@ -0,0 +1,51 @@
+class Solution {
+    public void solve(char[][] board) {
+        if(board==null || board.length==0)
+        {
+            return;
+        }
+        int m=board.length, n=board[0].length;
+
+        for(int i=0;i<m;i++)
+        { 
+            for(int j=0;j<n;j++)
+            {
+                if((i==0 || i==m-1 || j==0 || j==n-1 ) && board[i][j]=='O')
+                {
+                    dfs(board,i,j);
+                }
+            }
+        }
+
+        ///capturing of souurounding regions and revert T
+
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(board[i][j]=='O')
+                {
+                    board[i][j]='X';
+                }
+                else if(board[i][j]=='T')
+                {
+                    board[i][j]='O';
+                }
+            }
+        }
+    }
+    public void dfs(char[][] board, int i,int j)
+    {
+        if(i<0 || i>=board.length || j<0 || j>=board[0].length || board[i][j] != 'O')
+        {
+            return;
+        }
+
+        board[i][j]='T';
+
+        dfs(board, i+1,j);
+        dfs(board, i-1,j);
+        dfs(board, i,j+1);
+        dfs(board, i,j-1);
+    }
+}
